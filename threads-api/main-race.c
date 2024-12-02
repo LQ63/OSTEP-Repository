6f1db5e1d73176d51e2e7865032dce397eaa2b8d
LQ63 <lkhermann@web.de>
2024-12-02 23:05:05 +0100

    Kapitel 26/27

--- threads-api/main-race.c
+++ threads-api/main-race.c
@@ -1,18 +1,44 @@
 #include <stdio.h>
-
+#include <pthread.h>
 #include "common_threads.h"
 
+pthread_mutex_t lock;
 int balance = 0;
 
+void pTTthread_mutex_lock(pthread_mutex_t *lock) {
+    int rc = pthread_mutex_lock(lock);
+    assert(rc == 0); // terminiert das programm wenn rc == 0 nicht erfüllt ist
+}
+void pTTthread_mutex_init(pthread_mutex_t *lock) {
+    int rc = pthread_mutex_init(lock, NULL);
+    assert(rc == 0);
+}
+void pTTthread_mutex_unlock(pthread_mutex_t *lock) {
+    int rc = pthread_mutex_unlock(lock);
+    assert(rc == 0);
+}
+void pTTthread_mutex_destroy(pthread_mutex_t *lock) {
+    int rc = pthread_mutex_destroy(lock);
+    assert(rc == 0);
+}
+
+
 void* worker(void* arg) {
-    balance++; // unprotected access 
+    pTTthread_mutex_lock(&lock);
+    balance++; // unprotected access
+    pthread_mutex_unlock(&lock);
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
+    pTTthread_mutex_init(&lock);
     pthread_t p;
     Pthread_create(&p, NULL, worker, NULL);
+    pTTthread_mutex_lock(&lock);
     balance++; // unprotected access
+    pTTthread_mutex_unlock(&lock);
     Pthread_join(p, NULL);
+    pTTthread_mutex_destroy(&lock);
+    //printf("Balance: %d\n",balance);
     return 0;
 }
--- vm-beyondphys-policy/aufgabe3-trace.py
+++ vm-beyondphys-policy/aufgabe3-trace.py
@@ -19,9 +19,9 @@ def generatetrace(size=10, range1=6, locality=False):
 
 def main():
     parser = OptionParser()
-    parser.add_option('-s', '--size', default='10',   help='size of trace',  action='store', type='int', dest='size')
-    parser.add_option('-r', '--range', default='6',   help='range of pages', action='store', type='int', dest='range1')
-    parser.add_option('-l', '--locality', default='False',    help='set locality',action='store', type='Boolean', dest='locality')
+    parser.add_option('-s', '--size', default='10', help='size of trace',  action='store', type='int', dest='size')
+    parser.add_option('-r', '--range', default='6', help='range of pages', action='store', type='int', dest='range1')
+    parser.add_option('-l', '--locality', default='False', help='set locality',action='store', dest='locality')
     (options, args) = parser.parse_args()
 
     trace = generatetrace(size=options.size, range1=options.range1, locality=options.locality)
